5d8f8ad2acca2d8117dbf11e83c1547a3844d3e5
deadkorskiy <Deadkorskiy@gmail.com>
2018-10-17 05:17:39 +0300

    restart openvpn server throw api

--- src/endpoint/openvpn/management.py
+++ src/endpoint/openvpn/management.py
@@ -51,3 +51,44 @@ def load_stats():
             os.remove(output_tmp_file_name)
         except Exception as e2:
             logging.getLogger(__file__).error('Error during remove {}:{}'.format(output_tmp_file_name, str(e2)))
+
+
+@openvpn_management_bp.route("/restart/", methods=['GET'])
+@auth_required
+def restart():
+    """Кол-во подклченных пользователей"""
+    output = ''
+    output_tmp_file_name = os.path.join(
+        settings.TMP_OUTPUT_DIR,
+        'tmp_restart_output_{}.txt'.format(str(uuid.uuid4()))
+
+    )
+    cmd = """timeout 120 bash -c 'supervisorctl restart openvpn > {}'""".format(
+        output_tmp_file_name
+    )
+    try:
+        with fabric_settings(abort_exception=Exception):
+            try:
+                # Эта штука работает, но всегда падает из-за exit и timeout по этому выхлоп вытягивается через файл
+                local(cmd)
+            finally:
+                with open(output_tmp_file_name, 'r') as f:
+                    output = f.read()
+
+        return json_custom_response(
+            data={
+                'raw': str(output)
+            },
+            code=200
+        )
+    except Exception as e1:
+        logging.getLogger(__file__).error('Error during restart {}:{}'.format(output_tmp_file_name, str(e1)))
+        return json_custom_response(
+            errors_occured=[{'message': 'Restart error', 'Internal error': str(e1)}],
+            code=500
+        )
+    finally:
+        try:
+            os.remove(output_tmp_file_name)
+        except Exception as e2:
+            logging.getLogger(__file__).error('Error during remove {}:{}'.format(output_tmp_file_name, str(e2)))
